2c76a69cec6d556bd9b43d8507b53ef0029bf0e8
ProgrammerMaryam <72169698+mariamtahasaeed@users.noreply.github.com>
2026-06-09 10:37:16 -0600

    just click detecting

--- entities/potion/potion_1g.gd
+++ entities/potion/potion_1g.gd
@@ -1,33 +1,12 @@
 extends Area2D
+#This detects when the mouse is clicked on this specific scene and whatever inherits from this scene 
+func _unhandled_input(click: InputEvent) -> void:
+	if click is InputEventMouseButton and click.button_index == MOUSE_BUTTON_LEFT:
+		if click.pressed:
+			print("clciked")
+		else:
+			print("released")
 
-var dragging := false
-var drag_offset := Vector2.ZERO
-
-func _ready():
-	pass
-
-func _process(delta: float) -> void:
-	var direction := Vector2(0, 0) 
-	direction.x = Input.get_axis("mouse_clicked", "mouse_exit")
-	direction.y = Input.get_axis("mouse_exit", "mouse_clicked")
+				
+				
 	
-
-var mouse_over := false
-
-func _on_mouse_entered():
-	print("mouse entered")
-	mouse_over = true
-	
-func _on_mouse_exited():
-	print("mouse exited")
-	mouse_over = false
-
-
-func _input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT:
-			if event.pressed:
-				dragging = true
-				drag_offset = global_position - get_global_mouse_position()
-			else:
-				dragging = false
